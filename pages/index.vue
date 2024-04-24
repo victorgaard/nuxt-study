5113911d2cdca3aec99c391360b4837eaebc740a
Victor F. Santos <victor.gaard@gmail.com>
2024-04-24 18:50:26 +0200

    feat: syncronize search state with url query params

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import useProducts from '~/services/useProducts';
-const { products, pending, error, refresh } = useProducts()
-const search = ref('')
+const router = useRouter()
+const route = useRoute()
+
+const query = route.query.search?.toString() || ''
+const search = ref<string>(query)
+watch(search, () => router.push({ query: search.value ? { search: search.value } : {} }))
+
+const { products, pending, error, refresh } = await useProducts()
 const filteredProducts = computed(() => filterProducts({ productsArray: products.value, searchTerm: search.value }))
 </script>
 
@@ -12,10 +18,13 @@ const filteredProducts = computed(() => filterProducts({ productsArray: products
 
     <div v-else-if="pending">Loading...</div>
 
-    <div v-else class="flex flex-col gap-8">
-        <input v-model="search" placeholder="Search for your coffee.."
+    <div v-else class="flex flex-col gap-6">
+        <input v-model="search" placeholder="Search for coffee, location, flavor profile..."
             class="bg-slate-800 border text-sm border-slate-700 px-4 py-2.5 rounded-xl" />
-        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-12">
+        <div v-if="filteredProducts?.length === 0">
+            There are no results for {{ search }}
+        </div>
+        <div v-else class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-12">
             <ProductCard v-for="product in filteredProducts" :id="product.id" :name="product.name"
                 :description="product.description" :image_url="product.image_url" :price="product.price"
                 :region="product.region" :flavor_profile="product.flavor_profile" :weight="product.weight"
--- services/useProducts.ts
+++ services/useProducts.ts
@@ -1,6 +1,6 @@
 import type { Product } from "~/types/global"
 
-export default function useProducts() {
-    const { data: products, pending, error, refresh } = useFetch<Product[]>('https://fake-coffee-api.vercel.app/api')
+export default async function useProducts() {
+    const { data: products, pending, error, refresh } = await useFetch<Product[]>('https://fake-coffee-api.vercel.app/api')
     return { products, pending, error, refresh }
 }
