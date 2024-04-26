8ea6f3331d9d8b08bf8fc81aa9d3bf22e12faab8
Victor F. Santos <victor.gaard@gmail.com>
2024-04-26 11:09:07 +0200

    chore: install vue radix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'en'
-      }
-    },
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
-})
+	devtools: { enabled: true },
+	modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "radix-vue/nuxt"],
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
+		},
+		pageTransition: { name: "page", mode: "out-in" },
+	},
+});
